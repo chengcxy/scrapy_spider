5500b8f8c79a406fa3a77f948fcf515c2c87d416
chengxinyao <xinyao.cheng@zerone.com.cn>
2021-05-27 19:51:24 +0800

    delete

--- soybase/main.py
+++
@@ -1,8 +0,0 @@
-from scrapy.cmdline import execute
-from soybase.pipelines import SoybasePipeline
-
-if __name__ == '__main__':
-    soypipline = SoybasePipeline()
-    info = soypipline.create_table()
-    print(info)
-    execute("scrapy crawl soybase".split())
